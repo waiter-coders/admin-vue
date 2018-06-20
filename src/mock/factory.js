f0bdb504ed784d778ae4ad247455f91efe892646
韩剑 <916117482@qq.com>
2018-06-20 09:59:02 +0800

    eslint

--- src/mock/factory.js
+++ src/mock/factory.js
@@ -1,64 +1,69 @@
 import Mock from 'mockjs'
 
-const List = [];
-const count = 10;
-	var obj = {
-		'id|+1': 1,
-		'icon|+1': [ 'all', 'account', 'form', 'category'],
-		'path': '@url',
-		'name': '@string("lower", 3, 9)',
-		'title': '@ctitle(5, 9)'
-	};
+const List = []
+const count = 10
+var obj = {
+  'id|+1': 1,
+  'icon|+1': [ 'all', 'account', 'form', 'category'],
+  'path': '@url',
+  'name': '@string(https://shop.vivo.com.cn/product/10390?cid=gw_x_floor02, 3, 9)',
+  'title': '@ctitle(5, 9)'
+}
 for(let i = 0; i < count; i++){
 
-	List.push(Mock.mock({
-		'id|+1': 1,
-		'icon|+1': [ 'all', 'account', 'form', 'category'],
-		'path': '@url',
-		'name': '@string("lower", 3, 9)',
-		'title': '@ctitle(5, 9)',
-		'subMenus':{
-			'id': 1,
-			'icon': 'all',
-			'path': 'url',
-			'name': 'asdsd',
-			'title': '你好吗',
-		}
-	}));
+  List.push(Mock.mock({
+    'id|+1': 1,
+    'icon|+1': [ 'all', 'account', 'form', 'category'],
+    'path': '@url',
+    'name': '@string("lower", 3, 9)',
+    'title': '@ctitle(5, 9)',
+    'subMenus':{
+      'id': 1,
+      'icon': 'all',
+      'path': 'url',
+      'name': 'asdsd',
+      'title': '你好吗',
+    }
+  }))
 }
 //返回比较规范的列表，可以在这里提供多种方法，提供对menu参数的访问，该模块的数据，不仅仅用于刷新Menu,而且用户其他地方获取该板块的相关信息？
 export default {
-	getConfigs: () =>{
-		return {
-			data: [{
-				"type":"list",
-				"fields":[
-					{"name":"id", "field":"id", "type":"int", "primaryKey":true, "fastEdit":false},
-					{"name":"姓名", "field":"username", "primaryKey":false, "fastEdit":true},
-					{"name":"性别", "field":"sex", "type":"enum", "enum":{"1":"男","2":"女"}}
-				],
-				"search" : [
-					{"field":"username", "type":"text", "value": "", "inputTip":"请输入姓名"},
-					{"field":"sex","type":"enum", "enum":{"0":"请选择", "1":"男","2":"女"}, "value":"0", "inputTip":"请选择性别"}
-				],
-				"publicActions":[
-					{"name":"添加", "type":"page", "url":"/record/paging/add", "callback":"add",  "isDisabled":false},
-					{"name":"批量删除", "type":"ajax", "url":"/paging/list", "isDisabled":false},
-					{"name":"下载", "type":"callback",  "callback":"download",  "isDisabled":false}
-				],
-				"itemActions":[
-					{"name":"编辑", "type":"page", "url":"/record/paging/edit", "callback":"edit", "isDisabled":false},
-					{"name":"删除", "type":"ajax", "url":"/record/paging/delete", "isDisabled":false, "confirm":true},
-					{"name":"详情", "type":"page", "url":"/record/paging/show", "isDisabled":true}
-				],
-				"batchActions":[
-					{"name":"批量添加", "type":"ajax", "url":"", "isDisabled":false}
-				],
-				"paging":{}
-			  }],
-           	//data: List,
-			code: 0,
-			msg: 'success'
-		};
-	} 
+  getConfigs: () =>{
+    return {
+      data: [
+        {
+          'type':'list',
+          'fields':[
+            {'name':'id', 'field':'id', 'type':'int', 'primaryKey':true, 'fastEdit':false},
+            {'name':'姓名', 'field':'username', 'primaryKey':false, 'fastEdit':true},
+            {'name':'性别', 'field':'sex', 'type':'enum', 'enum':{'1':'男','2':'女'}}
+          ],
+          'search' : [
+            {'field':'username', 'type':'text', 'value': '', 'inputTip':'请输入姓名'},
+            {'field':'sex','type':'enum', 'enum':{'0':'请选择', '1':'男','2':'女'}, 'value':'0', 'inputTip':'请选择性别'}
+          ],
+          'publicActions':[
+            {'name':'添加', 'type':'page', 'url':'/record/paging/add', 'callback':'add',  'isDisabled':false},
+            {'name':'批量删除', 'type':'ajax', 'url':'/paging/list', 'isDisabled':false},
+            {'name':'下载', 'type':'callback',  'callback':'download',  'isDisabled':false}
+          ],
+          'itemActions':[
+            {'name':'编辑', 'type':'page', 'url':'/record/paging/edit', 'callback':'edit', 'isDisabled':false},
+            {'name':'删除', 'type':'ajax', 'url':'/record/paging/delete', 'isDisabled':false, 'confirm':true},
+            {'name':'详情', 'type':'page', 'url':'/record/paging/show', 'isDisabled':true}
+          ],
+          'batchActions':[
+            {'name':'批量添加', 'type':'ajax', 'url':'', 'isDisabled':false}
+          ],
+          'paging':{}
+        },
+        {
+          'type': 'form'
+        }
+      ],
+      //data: List,
+      code: 0,
+      msg: 'success'
+    }
+  } 
 }
--- src/router/index.js
+++ src/router/index.js
@@ -18,69 +18,67 @@ Vue.use(Router)
 
 // 页面路由， 公共路由在后面，自定义的页面路由添加到公共视图路由前，以便覆盖公共路由
 const router = new Router({
-    routes: [
-        // 无菜单页面路由        
+  routes: [
+    // 无菜单页面路由        
+    {
+      path: '/admin/login',
+      name: 'login',
+      meta: { 
+        title: '登录'
+      },
+      component: Login
+    },
+    // 主菜单路由
+    {
+      path: '/',
+      // name: 'main', 父级路由不能有name
+      component: Main,
+      meta: {
+        title: '首页'
+      },
+      children: [                
+        // 自定义页面路由
         {
-            path: '/admin/login',
-            name: 'login',
-            meta: { 
-                title: '登录'
-            },
-            component: Login
+          path: '/',
+          name: 'home',
+          meta: {
+            title: '首页'
+          },
+          component: Home
+        },
+        {
+          path: '/admin/account',
+          name: 'UserPassword',
+          meta: {
+            title: '管理员帐号'
+          },
+          component: AdminAccount
         },
 
-
-        // 主菜单路由
+        // 基于配置文件的工厂页面路由
         {
-            path: '/',
-            // name: 'main', 父级路由不能有name
-            component: Main,
-            meta: {
-            	title: '首页'
-            },
-	        children: [                
-                // 自定义页面路由
-                {
-                    path: '/',
-                    name: 'home',
-                    meta: {
-                            title: '首页'
-                    },
-                    component: Home
-                },
-                {
-                    path: '/admin/account',
-                    name: 'UserPassword',
-                    meta: {
-                            title: '管理员帐号'
-                    },
-                    component: AdminAccount
-                },
-
-                // 基于配置文件的工厂页面路由
-		        {
-		            path: '/:domain/:controller?/:subController?',
-		            name: 'factory',
-		            meta:{
-		            	title: '列表'
-		            },
-		            component: Factory,
-		            props:true
-		        }
-		    ]
-        },        
-    ]
+          path: '/:domain/:controller?/:subController?',
+          name: 'factory',
+          meta:{
+            title: '列表'
+          },
+          component: Factory,
+          props:true
+        }
+      ]
+    },        
+  ]
 })
 
 // 路由前置钩子，用于处理登录等
 router.beforeEach((to, from, next) => {
-    // 登录阻塞
-    /*if (to.path != '/user/login' && !isLogin()) {
-        return next({'path':'/user/login', 'replace':true})
-    }*/
+  // 登录阻塞
+  /*if (to.path != '/user/login' && !isLogin()) {
+      return next({'path':'/user/login', 'replace':true})
+  }*/
 
-    // 继续路由
-    next()
+  // 继续路由
+  next()
 })
 
 
