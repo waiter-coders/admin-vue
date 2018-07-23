539ac7d2538afec61bf355f460f7c7855f8cbe2f
tian.zheng <tian.zheng@zol.com.cn>
2018-07-23 15:24:18 +0800

    builder工具

---
+++ src/widgets/layout/Builder.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <component :is="config.type" :config="config" v-for="(config,index) in configs" v-bind:key="index">组件初始化失败</component>
+    </div>
+</template>
+
+<script>
+
+import {
+  AdminForm,
+  AdminList,
+  AdminTree,
+  AdminStatistics
+} from "@/widgets/admin";
+export default {
+  name: "factory",
+  components: {
+    AdminForm,
+    AdminList,
+    AdminTree,
+    AdminStatistics
+  },
+  props: ["configs"]
+};
+</script>
+
+<style lang="scss">
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+  .main-container {
+    min-height: 100%;
+    margin-left: 180px;
+    transition: margin-left 0.28s;
+  }
+  .menu-wrapper {
+    width: 180px;
+  }
+  &.hideSidebar {
+    .main-container {
+      margin-left: 64px;
+    }
+    .menu-wrapper {
+      width: 64px;
+    }
+  }
+}
+</style>
